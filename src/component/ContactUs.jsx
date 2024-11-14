0427f6cf26259912934749489934b2443ea86f18
krishnaonly1loan <krishna@only1loan.com>
2024-11-14 10:28:46 +0530

    new

--- src/component/ContactUs.jsx
+++ src/component/ContactUs.jsx
@@ -148,7 +148,7 @@ const ContactUs = () => {
             }}
           >
             <Typography variant="h4" gutterBottom textAlign="center">
-              Get in Touch
+              Get in
             </Typography>
             
             <Grid container spacing={2}>
